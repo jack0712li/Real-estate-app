f55265f856e24eafc882ec095b978916a5a30860
18lis <jack0712li@gmail.com>
2023-11-29 04:07:04 -0500

    create the routers and controllers

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+    res.json({ message: 'Api router is working' });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+
+
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,3 +17,5 @@ const app = express();
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+app.use("/api/user", userRouter);
