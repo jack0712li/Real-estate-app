ba225fb356aadba2946fe3558f4a2cd4f638bd43
Ruixin Li <kazezakimiyu@gmail.com>
2023-12-02 11:10:04 -0500

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
-
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
@@ -16,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
