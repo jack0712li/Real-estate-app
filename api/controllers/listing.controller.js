1f91a7ea420a83e3a1a09f855570eff4f3e72d1c
18lis <jack0712li@gmail.com>
2023-12-05 22:55:20 -0500

    complete delete user listing function

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,13 +1,31 @@
 
 import Listing from '../models/listing.model.js';
 
-
-
 export const createListing = async (req, res, next) => {
-    try {
-      const listing = await Listing.create(req.body);
-      return res.status(201).json(listing);
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only delete your own listings!'));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json('Listing has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
