7f292802393176e19cb9300be367bc175da9d0e5
MsQQQQQ <ellenqiyalun@gmail.com>
2023-12-06 17:18:01 -0500

    Add contact landlord function to listing page

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,36 +8,36 @@ export const test = (req, res) => {
         message: 'Api route is working!',
     });
 };
-  
+
 export const updateUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+        return next(errorHandler(401, 'You can only update your own account!'));
     try {
         if (req.body.password) {
             req.body.password = bcryptjs.hashSync(req.body.password, 10);
         }
-    
+
         const updatedUser = await User.findByIdAndUpdate(
             req.params.id,
             {
-            $set: {
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar,
-            },
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    avatar: req.body.avatar,
+                },
             },
             { new: true }
         );
-    
+
         const { password, ...rest } = updatedUser._doc;
-    
+
         res.status(200).json(rest);
     } catch (error) {
         next(error);
     }
 };
-  
+
 export const deleteUser = async (req, res, next) => {
     if (req.user.id !== req.params.id)
         return next(errorHandler(401, 'You can only delete your own account!'));
@@ -52,15 +52,24 @@ export const deleteUser = async (req, res, next) => {
 
 export const getUserListings = async (req, res, next) => {
     if (req.user.id === req.params.id) {
-      try {
-        const listings = await Listing.find({ userRef: req.params.id });
-        res.status(200).json(listings);
-      } catch (error) {
-        next(error);
-      }
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
     } else {
-      return next(errorHandler(401, 'You can only view your own listings!'));
+        return next(errorHandler(401, 'You can only view your own listings!'));
     }
-  };
+};
 
-  
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return next(errorHandler(404, 'User not found!'));
+        const { password: pass, ...rest } = user._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import {
@@ -12,6 +13,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -19,7 +21,9 @@ export default function Listing() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
   const params = useParams();
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -129,6 +133,15 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
