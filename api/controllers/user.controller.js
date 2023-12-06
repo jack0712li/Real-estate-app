30ffd03bd9ea85d79566257b3cffba93a4ce1938
18lis <jack0712li@gmail.com>
2023-12-05 23:18:40 -0500

    complete update listing api and ui

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -62,3 +62,5 @@ export const getUserListings = async (req, res, next) => {
       return next(errorHandler(401, 'You can only view your own listings!'));
     }
   };
+
+  
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import SignUp from './pages/SignUp';
 import Headers from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
+
 export default function App() {
   return <BrowserRouter>
   <Headers />
@@ -19,6 +21,7 @@ export default function App() {
       <Route element={<PrivateRoute />} >
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
       </Route>
     </Routes>
   </BrowserRouter>
