40b023b876a2f4c4968b2a38aeafe8cebdf29fa9
ZichuanZhu <zzc1922094069@gmail.com>
2023-12-07 05:57:26 -0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About NewBoston Estate</h1>
+      <p className='mb-4 text-slate-700'>NewBoston Estate is a leading real estate agency that specializes in helping clients buy, 
+        sell, and rent properties in the most desirable neighborhoods. 
+        Our team of experienced agents is dedicated to providing exceptional service 
+        and making the buying and selling process as smooth as possible.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        We have been in business for over 20 years and have helped thousands of people find their dream home. 
+        Our goal is to make your experience with us one that you will never forget!
+      </p>
+      <p className='mb-4 text-slate-700'>
+        We are committed to providing our clients with the best service possible. 
+        Our agents are knowledgeable about the local market and can help you find a property that meets your needs. 
+        If you are looking for a new home or want to sell your current one, we can help!
+      </p>
+      <p className='mb-4 text-slate-700'>
+        If you are looking for a new place to call home or want to sell your current property, 
+        please contact us today! We would love to hear from you!
+      </p>
+    </div>
   )
 }
