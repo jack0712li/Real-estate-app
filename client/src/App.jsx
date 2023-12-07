0e2c0abdcc5afa77aaacfb546f057ff6ed6618ec
ZichuanZhu <zzc1922094069@gmail.com>
2023-12-07 13:02:04 -0500

    Fix typo in listing.controller.js and App.jsx, make the rent&sale show all the listing items

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
-import Search from './pages/ Search';
+import Search from './pages/Search';
 
 export default function App() {
   return <BrowserRouter>
