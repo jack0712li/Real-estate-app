c139ee4ea128fe5457fda71cee7dce8cef1af4bc
ZichuanZhu <zzc1922094069@gmail.com>
2023-12-07 01:22:02 -0500

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -63,5 +63,46 @@ export const getListing = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-  
+
+  }
+
+  export const getListings = async (req, res, next) => {
+    try {
+      const limit = parseInt(req.query.limit) || 10;
+      const startIndex = parseInt(req.query.startIndex) || 0;
+
+      let offer = req.query.offer;
+      if (offer === undefined || offer === "false") {
+        offer = { $in: [false, true] };}
+
+      let furnished = req.query.furnished;
+      if (furnished === undefined || furnished === "false") {
+        furnished = { $in: [false, true] };}
+      
+      let parking = req.query.parking;
+      if (parking === undefined || parking === "false") {
+        parking = { $in: [false, true] };}
+
+      let type = req.query.type;
+      if (type === undefined || type === "all") {
+        type = { $in: ['sell', 'rent'] };}
+      
+      const searchTerm = req.query.searchTerm || "";
+
+      const sort = req.query.sort || "createdAt";
+
+      const order = req.query.order || "desc";
+
+      const listings = await Listing.find({
+        name: { $regex: searchTerm, $options: "i" }, // i means don't care about upeprcase or lowercase
+        offer,
+        furnished,
+        parking,
+        type,
+      }).sort({ [sort]: order }).limit(limit).skip(startIndex)
+
+      return res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
   }
