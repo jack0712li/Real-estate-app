846b53b34c155a35207cb660aec08ac0b593add0
18lis <jack0712li@gmail.com>
2023-12-07 22:14:38 -0500

    make setrole router private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,13 +22,13 @@ export default function App() {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
       <Route path="/search" element={<Search />} />
-      <Route path="/setrole" element={<Setrole />} />
 
       <Route path='/listing/:listingId' element={<Listing />} />
       <Route element={<PrivateRoute />} >
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-listing" element={<CreateListing />} />
         <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+        <Route path="/setrole" element={<Setrole />} />
       </Route>
     </Routes>
   </BrowserRouter>
