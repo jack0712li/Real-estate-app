487ad86cee0455a47223e9c67f8a6fd91104a80e
18lis <jack0712li@gmail.com>
2023-12-07 19:56:44 -0500

    finish the set role page ui

--- client/src/pages/SetRole.jsx
+++ client/src/pages/SetRole.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaUserTie, FaStore } from 'react-icons/fa';
 
 export default function ChooseRole() {
   const navigate = useNavigate();
@@ -17,16 +18,36 @@ export default function ChooseRole() {
         body: JSON.stringify({ type: role }),
       });
       console.log('Become a', role, 'successfully!');
-      navigate('/sign-in');  
+      navigate('/');  
     } catch (error) {
       console.error('Error updating role:', error);
     }
   };
-
   return (
-    <div className='choose-role'>
-      <button onClick={() => handleRoleSelection('buyer')}>Become a Buyer</button>
-      <button onClick={() => handleRoleSelection('seller')}>Become a Seller</button>
+    <div className='flex flex-col items-center justify-center h-screen gap-4 p-4'>
+      <div className='text-center'>
+        <h1 className='text-4xl font-bold text-gray-800'>
+          Welcome New User!
+        </h1>
+        <p className='text-xl text-gray-600 mt-2'>
+          What do you want to become?
+        </p>
+      </div>
+  
+      <div className='flex flex-wrap justify-center gap-8 mt-8'>
+        <div className='flex flex-col items-center w-1/2 max-w-sm p-8 border-2 border-gray-300 rounded-lg cursor-pointer hover:bg-blue-100 hover:border-blue-500 transition-all'
+             onClick={() => handleRoleSelection('buyer')}>
+          <FaUserTie className='text-8xl text-blue-600 mb-4' />
+          <span className='text-2xl text-gray-700'>Become a Buyer</span>
+        </div>
+  
+        <div className='flex flex-col items-center w-1/2 max-w-sm p-8 border-2 border-gray-300 rounded-lg cursor-pointer hover:bg-green-100 hover:border-green-500 transition-all'
+             onClick={() => handleRoleSelection('seller')}>
+          <FaStore className='text-8xl text-green-600 mb-4' />
+          <span className='text-2xl text-gray-700'>Become a Seller</span>
+        </div>
+      </div>
     </div>
   );
+  
 }
