52427c26ed9bfbf9a86c4bb07417d3d5ecffc7dc
ZichuanZhu <zzc1922094069@gmail.com>
2023-12-09 23:03:37 -0500

    Update link and text for Favourite List

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -441,7 +441,7 @@ export default function Profile() {
         {currentUser.type === "buyer" && (
           <Link
             className="bg-red-500 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 flex items-center gap-2 justify-center"
-            to={"/#"}
+            to={"/personal"}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -457,7 +457,7 @@ export default function Profile() {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </svg>
-            Create Favourite List
+            Show Your Favourite List
           </Link>
         )}
         {currentUser.type === "admin" && (
